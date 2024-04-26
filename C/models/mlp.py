a97bc4f71c4afe7324b8107bb8eac6474b1bc9be
alecerio <alessandrocerioliphd@gmail.com>
2024-04-26 18:53:33 +0200

    create mlp onnx model

---
+++ C/models/mlp.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(41, 32)
+        self.fc2 = nn.Linear(32, 2)
+    
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.softmax(self.fc2(x), dim=1)
+        return x
+
+model = MLP()
+mlp_input = torch.randn(1, 41, dtype=torch.float32)
+torch.onnx.export(model, mlp_input, "C/models/MLP.onnx", export_params=True, opset_version=14)
