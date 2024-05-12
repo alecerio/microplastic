ddc27a071f300c5890e2692e4f53cbead881423d
alecerio <alessandrocerioliphd@gmail.com>
2024-05-12 21:27:30 +0200

    optimize qgemm

--- C/experiments/gemm_quantization/include/qgemm.h
+++ C/experiments/gemm_quantization/include/qgemm.h
@@ -2,7 +2,8 @@
 #define __QGEMM__
 
 #include <stdint.h>
+#include "quant_helper.h"
 
-void qgemm(float Sx, int8_t Zx, int8_t* qw, float Sw, int32_t* qb, float Sy, int8_t Zy, int size_out, int size_in, int8_t* res, int8_t* qx);
+void qgemm(float SwSx_Sy, int8_t Zx, int8_t* qw, int32_t* qb, int8_t Zy, int size_out, int size_in, int8_t* res, int8_t* qx);
 
 #endif // __QGEMM__
--- C/experiments/gemm_quantization/src/qgemm.c
+++ C/experiments/gemm_quantization/src/qgemm.c
@@ -1,16 +1,21 @@
 #include "qgemm.h"
 
-void qgemm(float Sx, int8_t Zx, int8_t* qw, float Sw, int32_t* qb, float Sy, int8_t Zy, int size_out, int size_in, int8_t* res, int8_t* qx) {
-    int temp;
-    float tempf;
-    float c = (Sw * Sx) / Sy;
+void qgemm(float SwSx_Sy, int8_t Zx, int8_t* qw, int32_t* qb, int8_t Zy, int size_out, int size_in, int8_t* res, int8_t* qx) {
+    int32_t temp;
     for(int i=0; i<size_out; i++) {
         temp = 0;
         for(int j=0; j<size_in; j++) {
-            temp += qw[i*size_in+j] * (qx[j]-Zx);        
+            temp += (int32_t)qw[i*size_in+j] * ((int32_t)qx[j]-(int32_t)Zx);        
         }
         temp += qb[i];
-        tempf = c * (float)temp;
-        res[i] = (int8_t)tempf + Zy;
+        int32_t res_i = (int32_t)(SwSx_Sy * temp) + Zy;
+
+        #ifdef QUANT_DEBUG
+        CHECK_OVERFLOW_INT8(res_i)
+        #endif
+
+        CLIP_INT8(res_i)
+
+        res[i] = (int8_t)res_i;
     }
 }
