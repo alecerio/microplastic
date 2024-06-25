bacda05eae5c6d683c87c41c9a38f4645955eead
alecerio <alessandrocerioliphd@gmail.com>
2024-06-25 22:48:56 +0200

    add mcu deployment

---
+++ mcu_deployment/qgru/src/qgru_model.h
@@ -0,0 +1,16 @@
+// INCLUDE
+
+#include "ncutils.h"
+#include "ncquant.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+typedef float float32_t;
+
+// MACROS
+
+
+
+void run_inference(float32_t* tensor_onnxGemm_0, float32_t* tensor_onnxGemm_1, float32_t* tensor_output);
---
+++ mcu_deployment/qmlp/src/qmlp.h
@@ -0,0 +1,15 @@
+// INCLUDE
+
+#include "ncquant.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+typedef float float32_t;
+
+// MACROS
+
+
+
+void run_inference(float32_t* tensor_input, float32_t* tensor_output);
